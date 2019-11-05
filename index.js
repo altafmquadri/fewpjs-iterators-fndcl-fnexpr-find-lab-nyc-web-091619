66700264e0e17070111d0c47d8d01d99a5a7082d
Altaf Quadri <altafmquadri@gmail.com>
2019-11-04 22:37:00 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = function(record){
+  let year = record.find(function(res){
+    return res.result === 'W'
+  })
+  
+  return year = year ? year.year : undefined
+}
